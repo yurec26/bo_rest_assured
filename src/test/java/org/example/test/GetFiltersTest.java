c32c1af58da7d52a928b804930a0d49a8b5c2b6f
Iurii <yurasik96@inbox.ru>
2025-05-30 16:50:37 +0300

    added qase automation support

--- src/test/java/org/example/test/GetFiltersTest.java
+++ src/test/java/org/example/test/GetFiltersTest.java
@@ -1,88 +1,94 @@
-//package org.example.test;
-//
-//import io.qameta.allure.*;
-//import org.example.model.Todo;
-//import org.junit.jupiter.api.*;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.apache.http.HttpStatus.SC_CREATED;
-//import static org.apache.http.HttpStatus.SC_OK;
-//import static org.example.util.RandomTodoGenerator.generateRandomTodo;
-//import static org.example.util.RestAssuredHelper.*;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//@Tag("functional")
-//@Epic("Управление задачами")
-//@Feature("GET запросы с фильтрами")
-//@Severity(SeverityLevel.NORMAL)
-//@DisplayName("Проверка фильтрации по запросе GET")
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//public class GetFiltersTest {
-//
-//    private final List<Todo> TODO_LIST = new ArrayList<>();
-//    private final Integer TODO_NUMBERS_TO_POST = 5;
-//    private final Integer OFFSET_FILTER = 2;
-//    private final Integer LIMIT_FILTER = 2;
-//
-//
-//    @BeforeAll
-//    void setUP() {
-//        assertTrue(getTodos(SC_OK).isEmpty(),
-//                "БД должно быть пустым до тестового прогона");
-//        for (int i = 0; i < TODO_NUMBERS_TO_POST; i++) {
-//            TODO_LIST.add(generateRandomTodo());
-//        }
-//        TODO_LIST.forEach(s -> postTodo(s, SC_CREATED));
-//    }
-//
-//    @Test
-//    @DisplayName("Проверка фильтрации в запросе GET по фильтру offset")
-//    void testGetWithOffset() {
-//        List<Todo> todosWithOffset = getTodosOffset(OFFSET_FILTER, SC_OK);
-//        assertEquals(TODO_LIST.subList(OFFSET_FILTER, TODO_LIST.size()), todosWithOffset,
-//                "Фильтр 'offset %s' должен корректно отфильтровывать".formatted(OFFSET_FILTER));
-//    }
-//
-//    @Test
-//    @DisplayName("Проверка фильтрации в запросе GET по фильтру limit")
-//    void testGetWithLimit() {
-//        List<Todo> todosWithLimit = getTodosLimit(LIMIT_FILTER, SC_OK);
-//        assertEquals(TODO_LIST.subList(0, LIMIT_FILTER), todosWithLimit,
-//                "Фильтр 'limit %s' должен корректно отфильтровывать".formatted(LIMIT_FILTER));
-//    }
-//
-//    @Test
-//    @DisplayName("Проверка фильтрации в запросе GET по фильтрам offset и limit")
-//    void testGetWithOffsetAndLimit() {
-//        List<Todo> todosWithLimit = getTodosOffsetAndLimit(OFFSET_FILTER, LIMIT_FILTER, SC_OK);
-//        assertEquals(TODO_LIST.subList(OFFSET_FILTER, OFFSET_FILTER + LIMIT_FILTER), todosWithLimit,
-//                "Фильтры 'offset: %s  и limit: %s ' должен корректно отфильтровывать"
-//                        .formatted(OFFSET_FILTER, LIMIT_FILTER));
-//    }
-//
-//    @Test
-//    @DisplayName("Проверка нулевой фильтрации в запросе GET по фильтрам offset")
-//    void testGetWithZeroOffset() {
-//        List<Todo> todosWithOffset = getTodosOffset(0, SC_OK);
-//        assertEquals(TODO_LIST, todosWithOffset,
-//                "Фильтры 'offset: %s' должен корректно отфильтровывать"
-//                        .formatted(0));
-//    }
-//
-//    @Test
-//    @DisplayName("Проверка нулевой фильтрации в запросе GET по фильтрам limit")
-//    void testGetWithZeroLimit() {
-//        List<Todo> todosWithLimit = getTodosLimit(0, SC_OK);
-//        assertTrue(todosWithLimit.isEmpty(),
-//                "список должен быть пустой при limit = 0");
-//    }
-//
-//    @AfterAll
-//    @Step("Очищаем БД после прогона")
-//    void tearDown() {
-//        deleteAll();
-//    }
-//}
+package org.example.test;
+
+import io.qameta.allure.*;
+import io.qase.commons.annotation.QaseId;
+import org.example.model.Todo;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.example.util.RandomTodoGenerator.generateRandomTodo;
+import static org.example.util.RestAssuredHelper.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("functional")
+@Epic("Управление задачами")
+@Feature("GET запросы с фильтрами")
+@Severity(SeverityLevel.NORMAL)
+@DisplayName("Проверка фильтрации по запросе GET")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GetFiltersTest {
+
+    private final List<Todo> TODO_LIST = new ArrayList<>();
+    private final Integer TODO_NUMBERS_TO_POST = 5;
+    private final Integer OFFSET_FILTER = 2;
+    private final Integer LIMIT_FILTER = 2;
+
+
+    @BeforeAll
+    void setUP() {
+        assertTrue(getTodos(SC_OK).isEmpty(),
+                "БД должно быть пустым до тестового прогона");
+        for (int i = 0; i < TODO_NUMBERS_TO_POST; i++) {
+            TODO_LIST.add(generateRandomTodo());
+        }
+        TODO_LIST.forEach(s -> postTodo(s, SC_CREATED));
+    }
+
+    @QaseId(9)
+    @Test
+    @DisplayName("Проверка фильтрации в запросе GET по фильтру offset")
+    void testGetWithOffset() {
+        List<Todo> todosWithOffset = getTodosOffset(OFFSET_FILTER, SC_OK);
+        assertEquals(TODO_LIST.subList(OFFSET_FILTER, TODO_LIST.size()), todosWithOffset,
+                "Фильтр 'offset %s' должен корректно отфильтровывать".formatted(OFFSET_FILTER));
+    }
+
+    @QaseId(10)
+    @Test
+    @DisplayName("Проверка фильтрации в запросе GET по фильтру limit")
+    void testGetWithLimit() {
+        List<Todo> todosWithLimit = getTodosLimit(LIMIT_FILTER, SC_OK);
+        assertEquals(TODO_LIST.subList(0, LIMIT_FILTER), todosWithLimit,
+                "Фильтр 'limit %s' должен корректно отфильтровывать".formatted(LIMIT_FILTER));
+    }
+
+    @QaseId(11)
+    @Test
+    @DisplayName("Проверка фильтрации в запросе GET по фильтрам offset и limit")
+    void testGetWithOffsetAndLimit() {
+        List<Todo> todosWithLimit = getTodosOffsetAndLimit(OFFSET_FILTER, LIMIT_FILTER, SC_OK);
+        assertEquals(TODO_LIST.subList(OFFSET_FILTER, OFFSET_FILTER + LIMIT_FILTER), todosWithLimit,
+                "Фильтры 'offset: %s  и limit: %s ' должен корректно отфильтровывать"
+                        .formatted(OFFSET_FILTER, LIMIT_FILTER));
+    }
+
+    @QaseId(12)
+    @Test
+    @DisplayName("Проверка нулевой фильтрации в запросе GET по фильтрам offset")
+    void testGetWithZeroOffset() {
+        List<Todo> todosWithOffset = getTodosOffset(0, SC_OK);
+        assertEquals(TODO_LIST, todosWithOffset,
+                "Фильтры 'offset: %s' должен корректно отфильтровывать"
+                        .formatted(0));
+    }
+
+    @QaseId(13)
+    @Test
+    @DisplayName("Проверка нулевой фильтрации в запросе GET по фильтрам limit")
+    void testGetWithZeroLimit() {
+        List<Todo> todosWithLimit = getTodosLimit(0, SC_OK);
+        assertTrue(todosWithLimit.isEmpty(),
+                "список должен быть пустой при limit = 0");
+    }
+
+    @AfterAll
+    @Step("Очищаем БД после прогона")
+    void tearDown() {
+        deleteAll();
+    }
+}
--- src/test/java/org/example/test/HappyPathOrderedTestCase.java
+++ src/test/java/org/example/test/HappyPathOrderedTestCase.java
@@ -1,6 +1,7 @@
 package org.example.test;
 
 import io.qameta.allure.*;
+import io.qase.commons.annotation.QaseId;
 import org.example.annotation.TodoParam;
 import org.example.model.Todo;
 import org.example.util.TodoParamResolver;
@@ -9,8 +10,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
-import static org.apache.http.HttpStatus.SC_CREATED;
-import static org.apache.http.HttpStatus.SC_OK;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpStatus.*;
+import static org.example.util.RandomTodoGenerator.generateRandomTodo;
 import static org.example.util.RestAssuredHelper.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -36,8 +40,8 @@ public class HappyPathOrderedTestCase {
     }
 
     @Test
-    @TmsLink("TA-2")
     @Order(1)
+    @QaseId(2)
     @Story("Создание задачи POST todos/")
     @DisplayName("Проверка создания новой сущности в БД с валидными данными запроса.")
     @ExtendWith(value = TodoParamResolver.class)
@@ -48,50 +52,50 @@ public class HappyPathOrderedTestCase {
         assertEquals(testTodo, responsedTodo,
                 "В БД должна лежать одна сущность %s".formatted(testTodo));
     }
-//
-//    @Test
-//    @TmsLink("TA-3")
-//    @QaseId(3)
-//    @Order(2)
-//    @Story("Обновление задачи PUT todos/id")
-//    @DisplayName("Проверка замены сущности, уже лежащей  в БД, новой с валидными данными запроса.")
-//    @ExtendWith(value = TodoParamResolver.class)
-//    void putTodoTest(@TodoParam Todo newRandomTodo) {
-//        putTodo(testTodo.getId(), newRandomTodo, SC_OK);
-//        testTodo = newRandomTodo;
-//        Todo responsedTodo = getTodos(SC_OK).getFirst();
-//        assertEquals(testTodo, responsedTodo,
-//                "В БД должна лежать одна обновленная сущность %s".formatted(testTodo));
-//    }
-//
-//    @Test
-//    @TmsLink("TA-4")
-//    @QaseId(4)
-//    @Order(3)
-//    @Story("Удаление задачи DELETE todos/id")
-//    @DisplayName("Проверка удаления сущности из БД с валидными данными запроса.")
-//    void deleteTodoTest() {
-//        deleteTodo(testTodo.getId(), SC_NO_CONTENT);
-//        assertTrue(getTodos(SC_OK).isEmpty(),
-//                "В списке полученных сущностей не должно находиться сущности, которую мы удаляем.");
-//    }
-//
-//
-//    @Test
-//    @TmsLink("TA-1")
-//    @QaseId(1)
-//    @Story("Получение списка задач через GET /todos без фильтров")
-//    @DisplayName("Проверка запроса списка всех сущностей из БД без фильтров.")
-//    void getAllEntitiesTest() {
-//        List<Todo> todosRandom = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            todosRandom.add(generateRandomTodo());
-//        }
-//        todosRandom.forEach(s -> postTodo(s, SC_CREATED));
-//        List<Todo> respondedTodos = getTodos(SC_OK);
-//        assertEquals(todosRandom, respondedTodos,
-//                "В БД должны лежать именно эти сущности  %s".formatted(todosRandom));
-//    }
+
+    @Test
+    @TmsLink("TA-3")
+    @QaseId(3)
+    @Order(2)
+    @Story("Обновление задачи PUT todos/id")
+    @DisplayName("Проверка замены сущности, уже лежащей  в БД, новой с валидными данными запроса.")
+    @ExtendWith(value = TodoParamResolver.class)
+    void putTodoTest(@TodoParam Todo newRandomTodo) {
+        putTodo(testTodo.getId(), newRandomTodo, SC_OK);
+        testTodo = newRandomTodo;
+        Todo responsedTodo = getTodos(SC_OK).getFirst();
+        assertEquals(testTodo, responsedTodo,
+                "В БД должна лежать одна обновленная сущность %s".formatted(testTodo));
+    }
+
+    @Test
+    @TmsLink("TA-4")
+    @QaseId(4)
+    @Order(3)
+    @Story("Удаление задачи DELETE todos/id")
+    @DisplayName("Проверка удаления сущности из БД с валидными данными запроса.")
+    void deleteTodoTest() {
+        deleteTodo(testTodo.getId(), SC_NO_CONTENT);
+        assertTrue(getTodos(SC_OK).isEmpty(),
+                "В списке полученных сущностей не должно находиться сущности, которую мы удаляем.");
+    }
+
+
+    @Test
+    @TmsLink("TA-1")
+    @QaseId(1)
+    @Story("Получение списка задач через GET /todos без фильтров")
+    @DisplayName("Проверка запроса списка всех сущностей из БД без фильтров.")
+    void getAllEntitiesTest() {
+        List<Todo> todosRandom = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            todosRandom.add(generateRandomTodo());
+        }
+        todosRandom.forEach(s -> postTodo(s, SC_CREATED));
+        List<Todo> respondedTodos = getTodos(SC_OK);
+        assertEquals(todosRandom, respondedTodos,
+                "В БД должны лежать именно эти сущности  %s".formatted(todosRandom));
+    }
 
     //    @Test
     @AfterAll
